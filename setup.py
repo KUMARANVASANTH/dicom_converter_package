fc9c1d90b9e6b76f8d0bc1acc41faff7f974c4f8
K Vasantharan <kumarakurubaran944@gmail.com>
2023-10-30 19:07:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name='dicom_converter_package',
     version='0.1',
@@ -7,6 +9,6 @@ setup(
         'pydicom',
         'Pillow',
     ],
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
 )
