b1dfc4afa3ace4893cb57a0fabaffc635d2a9396
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2023-11-02 12:30:58 +0530

    package

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup, find_packages
 setup(
     name='dicom_converter_package',
     version='0.1',
+    author="Vasantharan K",
+    author_email="vasantharank.learn@gmail.com",
+    description="A package which converts all the images in a folder to JPEG file",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independant",
+    ],
     install_requires=[
         'pydicom',
         'Pillow',
     ],
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
 )
